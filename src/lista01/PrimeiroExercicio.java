b403885182a8a8231498e464c675c5188f4b9532
gumeeee <gaxco123@gmail.com>
2024-02-08 13:14:32 -0300

    Primeiro commit, junto com o primeiro exercicio

---
+++ src/lista01/PrimeiroExercicio.java
@@ -0,0 +1,32 @@
+package lista01;
+
+import java.util.Scanner;
+
+public class PrimeiroExercicio {
+
+	public static void main(String[] args) {
+		int a, b, c;
+		Scanner leitura = new Scanner(System.in);
+		
+		System.out.println("Digite o valor de A: ");
+		a = leitura.nextInt();
+		
+		System.out.println("Digite o valor de B: ");
+		b = leitura.nextInt();
+		
+		System.out.println("Digite o valor de C: ");
+		c = leitura.nextInt();
+		
+		if (a + b > c) {
+			System.out.println("A soma de A + B é maior do que C");
+		} else if (a + b < c) {
+			System.out.println("A soma de A + B é menor do que C");
+		} else {
+			System.out.println("A soma de A + B é igual a C");
+		}
+		
+		
+
+	}
+
+}
